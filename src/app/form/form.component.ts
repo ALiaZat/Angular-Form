43697954583ff1ccd190fc5e987ebb305a32e6db
Alia Zatari <zatarialia2000@hotmail.com>
2021-02-02 14:02:34 +0200

    second version

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,45 +1,58 @@
 import { Component, NgModule, OnInit, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-class Signup {
-  constructor(public username: string = '',
-              public gender: string = '',
-              public email: string = '',
-              public password: string = '',
-              public mobile: string = '') {
-  }
-}
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
+
 export class FormComponent implements OnInit {
 
-  email: any;
-  model: Signup = new Signup();
-
+  
+  submitted = false;
   profileForm = new FormGroup({
-    username: new FormControl('',[Validators.required]),
-    email: new FormControl('',[Validators.required, Validators.email]),
-    maleGender: new FormControl('',[Validators.required]),
-    femaleGender: new FormControl('',[Validators.required]),
-    avatar: new FormControl('',[Validators.required]),
-    mobile: new FormControl('',[Validators.required , Validators.min(10), Validators.max(10)]),
-    password: new FormControl('',[Validators.required]),
-    passwordConfirm: new FormControl('',[Validators.required]),
-  });
-  constructor() { }
+  username: new FormControl('',[Validators.required]),
+  email: new FormControl('',[Validators.required, Validators.email]),
+  gender: new FormControl('',[Validators.required]),
+  avatar: new FormControl('',[Validators.required]),
+  mobile: new FormControl('',[Validators.required ]), //,Validators.min(10), Validators.max(10)
+  password: new FormControl('',[Validators.required]),
+  passwordConfirm: new FormControl('',[Validators.required]),
+  },);
+  
+  constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  @ViewChild('f') form: any;
-
+  get f() { return this.profileForm.controls; }
+  
   onSubmit() {
-    if (this.form.valid) {
-      console.log("Form Submitted!");
-      this.form.reset();
-    }
+    // if ( this.submitted == true) {
+    //   console.log("Form Submitted!");
+    //   this.form.reset();
+    // }
   }
+  
 
+}
+
+
+export function MustMatch(controlName: string, matchingControlName: string) {
+  return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+          // return if another validator has already found an error on the matchingControl
+          return;
+      }
+
+      // set error on matchingControl if validation fails
+      if (control.value !== matchingControl.value) {
+          matchingControl.setErrors({ mustMatch: true });
+      } else {
+          matchingControl.setErrors(null);
+      }
+  }
 }
